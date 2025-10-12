8ce2c990f9d6ef163e603691b8e442d78bce3a0c
Viacheslav Korobka <korobka.vyacheslav@mail.ru>
2025-10-12 21:51:43 +0300

    change theme on isLightTheme

--- src/components/HomeScreen/homeScreen.jsx
+++ src/components/HomeScreen/homeScreen.jsx
@@ -4,16 +4,16 @@ import "./homeScreen.css";
 import { useState } from "react";
 
 const HomeScreen = ({ handlePage }) => {
-  const [theme, setTheme] = useState(true);
+  const [isLightTheme, setLightTheme] = useState(true);
 
   const handleChange = () => {
-    setTheme(!theme);
+    setLightTheme(!isLightTheme);
   };
 
   return (
     <div>
-      <div className={`home-wrapper ${theme ? "light" : "dark"}`}>
-        <ButtonChangeTheme handleChange={handleChange} theme={theme} />
+      <div className={`home-wrapper ${isLighttheme ? "light" : "dark"}`}>
+        <ButtonChangeTheme handleChange={handleChange} theme={isLightTheme} />
         <Button handlePage={handlePage} />
         <h1>Rock Paper Scissors The game</h1>
       </div>
